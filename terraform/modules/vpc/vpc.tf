c8ff6b285b6265082451cbb4e2ad4c6f5ac8175b
maishio <masanori.ishio119@gmail.com>
2023-04-11 17:55:54 +0900

    refactor: modified vpc module

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -1,75 +1,74 @@
-# --------------------------------------------------------------------------------
-# Amazon VPC モジュール
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
-# --------------------------------------------------------------------------------
-
 module "vpc" {
-  source     = "git::https://github.com/norishio2022/terraform-aws-resources.git//vpc"
+  source     = "../../resources/vpc"
   cidr_block = "10.0.0.0/16"
   tags       = var.tags
   vpc_name   = "${var.tags.service}-${var.tags.env}-vpc"
 }
 
-# --------------------------------------------------------------------------------
-# Amazon VPC Internet Gateway モジュール
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway
-# --------------------------------------------------------------------------------
-
 module "internet_gateway" {
-  source                = "git::https://github.com/norishio2022/terraform-aws-resources.git//internet_gateway"
+  source                = "../../resources/internet_gateway"
   internet_gateway_name = "${var.tags.service}-${var.tags.env}-igw"
   tags                  = var.tags
   vpc_id                = module.vpc.vpc.id
 }
 
-# --------------------------------------------------------------------------------
-# Amazon VPC Public Subnet モジュール
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
-# --------------------------------------------------------------------------------
-
 module "public_subnet" {
-  source      = "git::https://github.com/norishio2022/terraform-aws-resources.git//subnet"
-  cidr_blocks = ["10.0.0.0/24", "10.0.1.0/24"]
-  subnet      = "public"
-  tags        = var.tags
-  vpc_id      = module.vpc.vpc.id
+  source = "../../resources/subnet"
+  subnets = [
+    {
+      availability_zone = data.aws_availability_zones.this.names[0]
+      cidr_block        = "10.0.0.0/24"
+      name              = "${var.tags.alias}-${var.tags.env}-pub-${data.aws_availability_zones.this.names[0]}",
+    },
+    {
+      availability_zone = data.aws_availability_zones.this.names[1]
+      cidr_block        = "10.0.1.0/24"
+      name              = "${var.tags.alias}-${var.tags.env}-pub-${data.aws_availability_zones.this.names[1]}",
+    },
+  ]
+  tags   = var.tags
+  vpc_id = module.vpc.vpc.id
 }
-
-# --------------------------------------------------------------------------------
-# Amazon VPC Private Subnet モジュール
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
-# --------------------------------------------------------------------------------
 
 module "private_subnet" {
-  source      = "git::https://github.com/norishio2022/terraform-aws-resources.git//subnet"
-  cidr_blocks = ["10.0.10.0/24", "10.0.11.0/24"]
-  subnet      = "private"
-  tags        = var.tags
-  vpc_id      = module.vpc.vpc.id
+  source = "../../resources/subnet"
+  subnets = [
+    {
+      availability_zone = data.aws_availability_zones.this.names[0]
+      cidr_block        = "10.0.10.0/24"
+      name              = "${var.tags.alias}-${var.tags.env}-pri-${data.aws_availability_zones.this.names[0]}",
+    },
+    {
+      availability_zone = data.aws_availability_zones.this.names[1]
+      cidr_block        = "10.0.11.0/24"
+      name              = "${var.tags.alias}-${var.tags.env}-pri-${data.aws_availability_zones.this.names[1]}",
+    },
+  ]
+  tags   = var.tags
+  vpc_id = module.vpc.vpc.id
 }
-
-# --------------------------------------------------------------------------------
-# Amazon VPC NAT Gateway モジュール
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/nat_gateway
-# --------------------------------------------------------------------------------
 
 module "nat_gateway" {
-  source     = "git::https://github.com/norishio2022/terraform-aws-resources.git//nat_gateway"
-  subnet_ids = module.public_subnet.subnet.*.id
-  tags       = var.tags
+  source = "../../resources/nat_gateway"
+  nat_gateways = [
+    {
+      name      = "${var.tags.alias}-${var.tags.env}-ngw-${data.aws_availability_zones.this.names[0]}"
+      subnet_id = module.public_subnet.subnet.0.id
+    },
+    {
+      name      = "${var.tags.alias}-${var.tags.env}-ngw-${data.aws_availability_zones.this.names[1]}"
+      subnet_id = module.public_subnet.subnet.1.id
+    },
+  ]
+  tags = var.tags
 }
 
-# --------------------------------------------------------------------------------
-# Amazon VPC Public Route Table モジュール
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
-# --------------------------------------------------------------------------------
-
 module "public_route_table" {
-  source     = "git::https://github.com/norishio2022/terraform-aws-resources.git//route_table"
-  subnet     = "public"
-  subnet_ids = module.public_subnet.subnet.*.id
-  tags       = var.tags
-  vpc_id     = module.vpc.vpc.id
+  source           = "../../resources/route_table"
+  route_table_name = "${var.tags.alias}-${var.tags.env}-public"
+  subnet_ids       = module.public_subnet.subnet.*.id
+  tags             = var.tags
+  vpc_id           = module.vpc.vpc.id
   route = [
     {
       cidr_block = "0.0.0.0/0"
@@ -78,21 +77,30 @@ module "public_route_table" {
   ]
 }
 
-# --------------------------------------------------------------------------------
-# Amazon VPC Private Route Table モジュール（）
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
-# --------------------------------------------------------------------------------
-
-module "private_route_table" {
-  source          = "git::https://github.com/norishio2022/terraform-aws-resources.git//route_table"
-  nat_gateway_ids = module.nat_gateway.nat_gateway.*.id
-  subnet          = "private"
-  subnet_ids      = module.private_subnet.subnet.*.id
-  tags            = var.tags
-  vpc_id          = module.vpc.vpc.id
+module "private_route_table_1a" {
+  source           = "../../resources/route_table"
+  route_table_name = "${var.tags.alias}-${var.tags.env}-pri-${data.aws_availability_zones.this.names[0]}"
+  subnet_ids       = [module.private_subnet.subnet.0.id]
+  tags             = var.tags
+  vpc_id           = module.vpc.vpc.id
   route = [
     {
-      cidr_block = "0.0.0.0/0"
+      cidr_block     = "0.0.0.0/0"
+      nat_gateway_id = module.nat_gateway.nat_gateway.0.id
+    }
+  ]
+}
+
+module "private_route_table_1c" {
+  source           = "../../resources/route_table"
+  route_table_name = "${var.tags.alias}-${var.tags.env}-pri-${data.aws_availability_zones.this.names[1]}"
+  subnet_ids       = [module.private_subnet.subnet.1.id]
+  tags             = var.tags
+  vpc_id           = module.vpc.vpc.id
+  route = [
+    {
+      cidr_block     = "0.0.0.0/0"
+      nat_gateway_id = module.nat_gateway.nat_gateway.1.id
     }
   ]
 }
