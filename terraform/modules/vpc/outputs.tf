34ac9765434b04d948882a0c0c02035b434be1d4
maishio <masanori.ishio119@gmail.com>
2023-04-11 16:39:09 +0900

    refactor: add description to vpc output variables

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
-  value = module.vpc.vpc.id
+  description = "VPC ID"
+  value       = module.vpc.vpc.id
 }
 
 output "public_subnet_ids" {
-  value = module.public_subnet.subnet.*.id
+  description = "Public subnet IDs"
+  value       = module.public_subnet.subnet.*.id
 }
 
 output "private_subnet_ids" {
-  value = module.private_subnet.subnet.*.id
+  description = "Private subnet IDs"
+  value       = module.private_subnet.subnet.*.id
 }
