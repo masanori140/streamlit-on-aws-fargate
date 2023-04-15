b6318e975bb27b86d160cc7d369f93a6e09b4d8b
maishio <masanori.ishio119@gmail.com>
2023-04-15 13:29:34 +0900

    refactor: add hostzone variable

--- terraform/modules/acm/variables.tf
+++ terraform/modules/acm/variables.tf
@@ -1,3 +1,8 @@
+variable "hostzone" {
+  description = "A map of Route53 Hosted Zone to create the DNS record in."
+  type        = map(string)
+}
+
 variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
