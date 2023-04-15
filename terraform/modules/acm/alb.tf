698409a22b16abf32fb3c99f88147190ba98323e
maishio <masanori.ishio119@gmail.com>
2023-04-15 13:28:50 +0900

    refactor: add tags variable to the module

--- terraform/modules/acm/alb.tf
+++ terraform/modules/acm/alb.tf
@@ -1,6 +1,7 @@
 module "alb_acm" {
   source            = "../../resources/acm"
   domain_name       = data.aws_route53_zone.this.name
+  tags              = var.tags
   validation_method = "DNS"
   zone_id           = data.aws_route53_zone.this.zone_id
 }
--- terraform/modules/acm/cognito.tf
+++ terraform/modules/acm/cognito.tf
@@ -1,6 +1,7 @@
 module "cognito_acm" {
   source            = "../../resources/acm"
   domain_name       = "auth.${data.aws_route53_zone.this.name}"
+  tags              = var.tags
   validation_method = "DNS"
   zone_id           = data.aws_route53_zone.this.zone_id
 }
