0fb37bfaf873fbd84cd0b04432508bc088101008
maishio <masanori.ishio119@gmail.com>
2023-04-15 20:46:11 +0900

    refactor: modified value of the alb_target_group_arn

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -1,4 +1,4 @@
 output "alb_target_group_arn" {
   description = "ARN of the ALB target group."
-  value       = module.target_group.lb_target_group.arn
+  value       = module.target_group.elb_target_group.arn
 }
