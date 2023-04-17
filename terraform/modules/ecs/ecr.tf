78a104042a3184896dab1e61ba31f6a43e009048
maishio <masanori.ishio119@gmail.com>
2023-04-17 23:25:56 +0900

    refactor: Changed the value of the force_delete from false to true

--- terraform/modules/ecs/ecr.tf
+++ terraform/modules/ecs/ecr.tf
@@ -2,8 +2,9 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_repository
 
 module "ecr" {
-  source = "../../resources/ecs/ecr"
-  name   = "${var.tags.service}-${var.tags.env}-streamlit"
-  path   = "${path.module}/files/template/ecr_lifecycle_policy.json.tpl"
-  tags   = var.tags
+  source       = "../../resources/ecs/ecr"
+  force_delete = true
+  name         = "${var.tags.service}-${var.tags.env}-streamlit"
+  path         = "${path.module}/files/template/ecr_lifecycle_policy.json.tpl"
+  tags         = var.tags
 }
